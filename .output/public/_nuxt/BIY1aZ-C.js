5b13328de5da460e3a553f583050fbfaf5e040da
Raaydan <ilsur2000.2016@gmail.com>
2024-05-21 18:39:42 +0300

    second deploy

---
+++ .output/public/_nuxt/BIY1aZ-C.js
@@ -0,0 +1,1 @@
+import{_ as c,c as e,o as r}from"./gIJAkteV.js";const t={};function n(o,a){return r(),e("h1",null,"Корзина")}const _=c(t,[["render",n]]);export{_ as default};
---
+++ .output/public/_nuxt/CFnQ4vd4.js
@@ -0,0 +1,1 @@
+import{y as e,A as t}from"./gIJAkteV.js";const s=e({__name:"[category]",setup(n){return t(),(o,r)=>null}});export{s as default};
